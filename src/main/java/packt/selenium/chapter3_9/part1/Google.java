67ccb51465d146f4ddb5e5fb06b9efbdd92e05d6
Robert Long <robmlong@gmail.com>
2017-07-12 23:34:58 +0100

    Wrong title

--- src/main/java/packt/selenium/chapter3_9/part1/Google.java
+++ src/main/java/packt/selenium/chapter3_9/part1/Google.java
@@ -13,7 +13,7 @@ public class Google {
 		this.driver = driver;
 		this.baseURL = "http://www.google.com";
 		this.driver.get(baseURL + "/?gws_rd=cr&ei=y4FmWeTjLdDMwQLbo6CYBQ");
-		if(!driver.getTitle().equals("Google")){
+		if(!driver.getTitle().equals("Google-Selenium")){
 			try {
 				throw new Exception();
 			} catch (Exception e) {
